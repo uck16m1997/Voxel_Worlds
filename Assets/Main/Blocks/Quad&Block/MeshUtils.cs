9920efc6819b9da717ef2061c2999bafda77682c
uck16m1997 <uckaya6@gmail.com>
2022-07-13 21:58:11 +0300

    Quads and Blocks are created

---
+++ Assets/Main/Blocks/Quad&Block/MeshUtils.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// VertexData will hold the vertex vector, normal vector and the uv vector
+using VertexData = System.Tuple<UnityEngine.Vector3, UnityEngine.Vector3, UnityEngine.Vector2>;
+public static class MeshUtils
+{
+    public enum BlocType
+    {
+        GRASSTOP, GRASSSIDE, DIRT, WATER, STONE, SAND
+    }
+    // [,] means it is an two dimensional array
+    public static Vector2[,] blockUVs = {
+        /*GRASSTOP*/ {new Vector2(0.125f,0.375f), new Vector2(0.1875f,0.375f),new Vector2(0.125f,0.4375f),new Vector2(0.1875f,0.4375f)},
+        /*GRASSSIDE*/ {new Vector2(0.1875f,0.9375f), new Vector2(0.25f,0.9375f),new Vector2(0.1875f,1.0f),new Vector2(0.25f,1.0f)},
+        /*DIRT*/ {new Vector2(0.125f,0.9375f), new Vector2(0.1875f,0.9375f),new Vector2(0.125f,1.0f),new Vector2(0.1875f,1.0f)},
+        /*WATER*/ {new Vector2(0.875f,0.125f), new Vector2(0.9375f,0.125f),new Vector2(0.875f,0.1875f),new Vector2(0.9375f,0.1875f)},
+        /*STONE*/ {new Vector2(0.0f,0.875f), new Vector2(0.0625f,0.875f),new Vector2(0.0f,0.9375f),new Vector2(0.0625f,0.9375f)},
+        /*SAND*/ {new Vector2(0.125f,0.875f), new Vector2(0.1875f,0.875f),new Vector2(0.125f,0.9375f),new Vector2(0.1875f,0.9375f)}
+
+    };
+    public static Mesh MergeMeshes(Mesh[] meshes)
+    {
+        // Create a mesh to hold all the data
+        Mesh mesh = new Mesh();
+
+        // Will keep track of the order of vertices
+        // Ordering is important for triangle indexes
+        Dictionary<VertexData, int> pointsOrder = new Dictionary<VertexData, int>();
+        // Hold the data coming in and help reorder, append and remove duplicates
+        HashSet<VertexData> pointsHash = new HashSet<VertexData>();
+        List<int> tris = new List<int>();
+        // Tracker for the index we are currently working on
+        int pIndex = 0;
+        // loop through meshes
+        for (int i = 0; i < meshes.Length; i++)
+        {
+            // If there are null meshes continue
+            if (meshes[i] == null)
+            {
+                continue;
+            }
+            // loop through the vertices of the mesh
+            for (int j = 0; j < meshes[i].vertices.Length; j++)
+            {
+                Vector3 v = meshes[i].vertices[j];
+                Vector3 n = meshes[i].normals[j];
+                Vector2 uv = meshes[i].uv[j];
+                VertexData p = new VertexData(v, n, uv);
+
+                //Hash is faster for looking up if it is already existing then dictionary
+                if (!pointsHash.Contains(p))
+                {
+                    // add to dictionary and hashset
+                    pointsOrder.Add(p, pIndex);
+                    pointsHash.Add(p);
+                    // update seen index tracker
+                    pIndex++;
+                }
+            }
+
+            // Get the triangles that form the mesh
+            for (int t = 0; t < meshes[i].triangles.Length; t++)
+            {
+                // Each value contains the index value of the vertexes that form the triangle
+                int triPoint = meshes[i].triangles[t];
+
+                Vector3 v = meshes[i].vertices[triPoint];
+                Vector3 n = meshes[i].normals[triPoint];
+                Vector2 uv = meshes[i].uv[triPoint];
+                VertexData p = new VertexData(v, n, uv);
+
+                // If the vertex needed for the triangle is in the dictionary get its index value
+                int index;
+                pointsOrder.TryGetValue(p, out index);
+                // Add the vertex index
+                tris.Add(index);
+            }
+            // Delete the ith mesh 
+            meshes[i] = null;
+        }
+        ExtractArrays(pointsOrder, mesh);
+        mesh.triangles = tris.ToArray();
+        mesh.RecalculateBounds();
+        return mesh;
+    }
+
+    // Another class function to turn the dictionary to Vector3s
+    public static void ExtractArrays(Dictionary<VertexData, int> list, Mesh mesh)
+    {
+        List<Vector3> verts = new List<Vector3>();
+        List<Vector3> norms = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
+
+        foreach (VertexData v in list.Keys)
+        {
+            verts.Add(v.Item1);
+            norms.Add(v.Item2);
+            uvs.Add(v.Item3);
+        }
+        mesh.vertices = verts.ToArray();
+        mesh.normals = norms.ToArray();
+        mesh.uv = uvs.ToArray();
+    }
+}
---
+++ Assets/Main/Blocks/Quad&Block/Quad.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quad
+{
+    public Mesh mesh;
+    public Quad(Block.BlockSide side, Vector3 offset, MeshUtils.BlocType bType)
+    {
+        // // We can add mesh filter programtically 
+        // MeshFilter mf = this.gameObject.AddComponent<MeshFilter>();
+        // // We can add mesh renderer programtically
+        // MeshRenderer mr = this.gameObject.AddComponent<MeshRenderer>();
+
+        // Create a new mesh 
+        mesh = new Mesh();
+        mesh.name = "ScriptedQuad";
+
+        // Quad has 4 corners for vertices and 4 normals for each vertex
+        Vector3[] vertices = new Vector3[4];
+        Vector3[] normals = new Vector3[4];
+        // UV coordinates one for each vertex
+        Vector2[] uvs = new Vector2[4];
+        // Two polygons with 3 vertices for each
+        int[] polygons = new int[6];
+
+        Vector2 uv00 = MeshUtils.blockUVs[(int)bType, 0];
+        Vector2 uv01 = MeshUtils.blockUVs[(int)bType, 1];
+        Vector2 uv10 = MeshUtils.blockUVs[(int)bType, 2];
+        Vector2 uv11 = MeshUtils.blockUVs[(int)bType, 3];
+
+        // (X , Y ,Z)
+        // Positive X Left , Negative X Right
+        // Positive Y Up , Negative Y Down
+        // Positive Z Front , Negative Z Back
+
+        // Cube corners are 8 vertices
+        Vector3 p0 = new Vector3(-0.5f, -0.5f, 0.5f) + offset; // Front Bottom Right
+        Vector3 p1 = new Vector3(0.5f, -0.5f, 0.5f) + offset; // Front Bottom Left
+        Vector3 p2 = new Vector3(0.5f, -0.5f, -0.5f) + offset; // Back Bottom Left
+        Vector3 p3 = new Vector3(-0.5f, -0.5f, -0.5f) + offset; // Back Bottom Right 
+        Vector3 p4 = new Vector3(-0.5f, 0.5f, 0.5f) + offset; // Front Top Right
+        Vector3 p5 = new Vector3(0.5f, 0.5f, 0.5f) + offset; // Front Top Left
+        Vector3 p6 = new Vector3(0.5f, 0.5f, -0.5f) + offset; // Back Top Left
+        Vector3 p7 = new Vector3(-0.5f, 0.5f, -0.5f) + offset; // Back Top Right
+
+        switch (side)
+        {
+            case Block.BlockSide.FRONT:
+                {
+                    // Specific points we take all of them have positive Z which is their normal
+                    vertices = new Vector3[] { p5, p4, p0, p1 };
+                    normals = new Vector3[] { Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward };
+                    //  hmm
+                    uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                    // values are indexes for the vertices
+                    // 3 1 0 is the polygon formed by p0,p5,p4
+                    // 3 2 1 is the polygon formed by p0,p1,p5
+                    // It matters that each triangle is specified clockwise to determine which side will be visible
+                    polygons = new int[] { 0, 1, 2, 2, 3, 0 };
+                    break;
+                }
+            case Block.BlockSide.BACK:
+                {
+                    // Specific points we take all of them have positive Z which is their normal
+                    vertices = new Vector3[] { p6, p7, p3, p2 };
+                    normals = new Vector3[] { Vector3.back, Vector3.back, Vector3.back, Vector3.back };
+                    //  hmm
+                    uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                    // It matters that each triangle is specified clockwise to determine which side will be visible
+                    polygons = new int[] { 2, 1, 0, 0, 3, 2 };
+                    break;
+                }
+            case Block.BlockSide.LEFT:
+                {
+                    // Specific points we take all of them have positive Z which is their normal
+                    vertices = new Vector3[] { p6, p5, p1, p2 };
+                    normals = new Vector3[] { Vector3.left, Vector3.left, Vector3.left, Vector3.left };
+                    //  hmm
+                    uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                    // It matters that each triangle is specified clockwise to determine which side will be visible
+                    polygons = new int[] { 0, 1, 2, 2, 3, 0 };
+                    break;
+                }
+            case Block.BlockSide.RIGHT:
+                {
+                    // Specific points we take all of them have positive Z which is their normal
+                    vertices = new Vector3[] { p7, p4, p0, p3 };
+                    normals = new Vector3[] { Vector3.right, Vector3.right, Vector3.right, Vector3.right };
+                    //  hmm
+                    uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                    // It matters that each triangle is specified clockwise to determine which side will be visible
+                    polygons = new int[] { 2, 1, 0, 0, 3, 2 };
+                    break;
+                }
+            case Block.BlockSide.TOP:
+                {
+                    // Specific points we take all of them have positive Z which is their normal
+                    vertices = new Vector3[] { p6, p7, p4, p5 };
+                    normals = new Vector3[] { Vector3.up, Vector3.up, Vector3.up, Vector3.up };
+                    //  hmm
+                    uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                    // It matters that each triangle is specified clockwise to determine which side will be visible
+                    polygons = new int[] { 0, 1, 2, 2, 3, 0 };
+                    break;
+                }
+            case Block.BlockSide.BOTTOM:
+                {
+                    // Specific points we take all of them have positive Z which is their normal
+                    vertices = new Vector3[] { p2, p3, p0, p1 };
+                    normals = new Vector3[] { Vector3.down, Vector3.down, Vector3.down, Vector3.down };
+                    //  hmm
+                    uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                    // It matters that each triangle is specified clockwise to determine which side will be visible
+                    polygons = new int[] { 2, 1, 0, 0, 3, 2 };
+                    break;
+                }
+
+        }
+
+
+
+        mesh.vertices = vertices;
+        mesh.normals = normals;
+        mesh.uv = uvs;
+        mesh.triangles = polygons;
+
+        mesh.RecalculateBounds();
+        // mf.mesh = mesh;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
